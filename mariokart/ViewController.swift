2cb5330bda77cd5d8ffea5e47688239d66afb599
dnlfmkn <famakindaniel7@gmail.com>
2020-02-17 19:32:48 -0500

    Initial Commit

---
+++ mariokart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mariokart
+//
+//  Created by user160656 on 2/17/20.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
